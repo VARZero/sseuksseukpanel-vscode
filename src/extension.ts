62e85d2cfb5752bbf08092dd92679926083d2a70
VARZero <cydiazero@gmail.com>
2023-06-18 13:51:32 +0900

    ui 구성 얼추 완성?

--- src/extension.ts
+++ src/extension.ts
@@ -66,13 +66,12 @@ class SseukSseukPanel implements vscode.WebviewViewProvider{
 
 	public lineCursor(line: vscode.TextEditorSelectionChangeEvent){
 		if(this._view) {
-			this._view.webview.postMessage({ type: 'lineCurser', line: line });
+			this._view.webview.postMessage({ type: 'lineCurser', linePosition: line.selections });
 		}
 	}
 
 	private _getSSPHtmlWebview(webview: vscode.Webview) {
-		return `
-<!DOCTYPE html>
+		return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -138,6 +137,22 @@ body{
     flex: 1;
 }
 
+#SelToolBox{
+    display: none;
+}
+
+#SelToolBox.active{
+    display: block;
+}
+
+#InsToolBox{
+    display: none;
+}
+
+#InsToolBox.active{
+    display: block;
+}
+
 #ref{display: none;}
     </style>
     <title>SseukSsuek Panel</title>
@@ -159,8 +174,25 @@ body{
                 <div id="appendTab" class="appendEle"></div>
             </div>
         </div>
+        <div id="SelToolBox">
+            <div class="tools" id="cancel">취소</div>
+            <div class="tools" id="cut">자르기</div>
+            <div class="tools" id="copy">복사</div>
+            <div class="tools" id="delete">지우기</div>
+        </div>
+        <div id="InsToolBox">
+            <div class="tools" id="stop">중단</div>
+            <div class="tools" id="space">공백</div>
+            <div class="tools" id="tab">탭</div>
+        </div>
     </section>
     <section id="output">
+        <div id="process">
+            <div id="loading">
+                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_P7sC{fill:var(--elementC);transform-origin:center;animation:spinner_svv2 .75s infinite linear}@keyframes spinner_svv2{100%{transform:rotate(360deg)}}</style><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_P7sC"/></svg>
+            </div>
+        </div>
+        
         <div id="EditBox">
             <div id="strings">
                 <div id="textArea">
@@ -186,12 +218,45 @@ body{
                 <div id="appendTab" class="appendEle"></div>
             </div>
         </div>
+        <div id="SelToolBox">
+            <div class="tools" id="cancel">취소</div>
+            <div class="tools" id="cut">자르기</div>
+            <div class="tools" id="copy">복사</div>
+            <div class="tools" id="delete">지우기</div>
+        </div>
+        <div id="InsToolBox">
+            <div class="tools" id="stop">중단</div>
+            <div class="tools" id="AddSpace">
+                <div class="tools" id="space">공백</div>
+                <div class="tools" id="tab">탭</div>
+            </div>
+        </div>
     </section>
     <script>
-txt = "te xt"
-RefEb = document.querySelector("#sys > #EditBox");
+let txt = "te xt"
+let nowStatus = undefined;
+const RefEb = document.querySelector("#sys > #EditBox");
+
+class status{
+    constructor(){
+        this.active = false;
+        this.lineNum = -1;
+        this.col = -1;
+
+        this.select = false;
+        this.seletStart = -1;
+        this.selectEnd = -1;
+        
+        this.insert = false;
+        this.insertStart = -1;
+
+        this.append = false;
+    }
+}
 
 function setSsuekSsuek(text){
+    if (nowStatus != undefined){delete nowStatus;}
+    nowStatus = new status;
     newEb = RefEb.cloneNode(true);
     newEbTa = newEb.children[0].children[0]; newEbTc = newEbTa.children[0];
     newEbIa = newEb.children[0].children[1]; newEbIc = newEbTa.children[0];
@@ -234,7 +299,6 @@ function appsPD(e){
 function apptPD(e){
     //
 }
-
 
     </script>
 </body>
